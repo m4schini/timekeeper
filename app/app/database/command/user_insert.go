13414b197e0ad43446b9fb340dc20b8d232b3279
aur0ra <codeberg+aurora@schink.xyz>
2026-02-19 21:31:30 +0100

    fix: Upsert user updates login_name and password

--- app/app/database/command/user_insert.go
+++ app/app/database/command/user_insert.go
@@ -20,7 +20,7 @@ func (c *UpsertUserHandler) Execute(m UpsertUserRequest) (id int, err error) {
 	row := c.DB.QueryRow(`
 INSERT INTO raumzeitalpaka.users (id, login_name, display_name, password, role) 
 VALUES ($1, $2, $3, $4, $5)
-ON CONFLICT (id) DO UPDATE SET role = $5, display_name = $3
+ON CONFLICT (id) DO UPDATE SET login_name = $2, display_name = $3, password = $4, role = $5
 RETURNING id`, m.ID, m.LoginName, m.DisplayName, m.PasswordHash, m.Role)
 	if err = row.Err(); err != nil {
 		return m.ID, err
